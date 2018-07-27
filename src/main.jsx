9e1b11d8ab1a75a0768ff614d70fca2644d1219e
Joel Shaw <jshaw@rvadesignsolutions.com>
2018-07-26 20:42:00 -0400

    Create base React files

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/style.css';
+
+import Test from './components/test';
+
+ReactDOM.render(
+    <Test />,
+    document.getElementById('app')
+);

b6e92ac5edef29b1f79dc22e6306dbd45bce7191
joelshaw <jshaw@rvadesignsolutions.com>
2018-07-29 16:04:13 -0400

    Added lorem ipsum to illustrate content

--- src/components/content.js
+++ src/components/content.js
@@ -3,13 +3,7 @@ import React, { Component } from 'react';
 class Content extends Component {
     render(){
         return(
-            <div className="drawerContainer">
-                <div className="drawerHeader">
-                    <h1>{this.props.name}</h1>
-                </div>
-                <div className="drawerContent">
-                </div>
-            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et sapien maximus, eleifend risus dapibus, ultricies dui. Aliquam erat volutpat. Vestibulum vel diam sed tortor aliquet mattis. Maecenas est nulla, rutrum eget lectus eget, elementum euismod diam. Phasellus odio turpis, convallis nec enim vel, maximus blandit orci. Donec vitae quam vel libero eleifend gravida id nec libero. Vivamus ut diam sit amet mauris mattis mollis. Nulla facilisi. Sed a ultricies nulla. Nullam ut dignissim nulla, sit amet vestibulum lectus. Proin a ligula viverra, ullamcorper purus mattis, accumsan quam.</p>
         );
     }
 }

26d77f43263b0c121c2945c4e350d9c43181921e
joelshaw <jshaw@rvadesignsolutions.com>
2018-07-29 16:05:12 -0400

    Added 'onClick' functionality to the drawer && added 'content' component to illustrate dynamically generated content

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
 
-// import Content from './components/content';
+import Content from './content';
 
 class Drawer extends Component {
+   constructor(props){
+       super(props);
+       this.state = {
+           active: false,
+       };
+       this.state.value = "+";
+
+       this.handleClick = this.handleClick.bind(this);
+   }
+
+    handleClick(event){
+        this.setState (drawerFunction => ({
+            isToggleOn: !drawerFunction.isToggleOn
+        }));
+    }
+    drawer(props){
+        const isToggleOn = props.isToggleOn;
+        if(isToggleOn){
+            console.log('yes');
+        }
+        console.log('no');
+    }
     render(){   
         return(
-            <div className="drawerContainer" aria-expanded="true">
+            <div className="drawerContainer">
                 <div className="drawerHeader">
                     <h1>{this.props.name}</h1>
-                    <button className="drawerState"></button>
+                    {this.state.isToggleOn ? 'NO' : 'YES'}
+                    <button className="drawerState" value="+" onClick={this.handleClick}>{this.state.value}</button>
                 </div>
                 <div className="drawerContent">
-                    <p>content</p>
+                    <Content />
                 </div>
             </div>
         );

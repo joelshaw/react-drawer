f29cbb6acefa645849111aea5d1706e252d6c5b9
joelshaw <jshaw@rvadesignsolutions.com>
2018-07-29 16:39:48 -0400

    Update drawer indicator state on click

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -6,7 +6,7 @@ class Drawer extends Component {
    constructor(props){
        super(props);
        this.state = { show: true };
-       this.state.value = "+";
+       this.state.value = "-";
 
        this.handleClick = this.handleClick.bind(this);
    }
@@ -23,7 +23,7 @@ class Drawer extends Component {
             <div className="drawerContainer">
                 <div className="drawerHeader">
                     <h1>{this.props.name}</h1>
-                    <button className="drawerState" value="+" onClick={this.handleClick}>{this.state.value}</button>
+                    <button className="drawerState" value="+" onClick={this.handleClick}>{ this.state.isClosed ? ( this.state.value ) : '+' }</button>
                 </div>
                 <div className="drawerContent">
                     { this.state.isClosed && <Content /> }

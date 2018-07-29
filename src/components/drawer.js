5ec5b0c2a3825ab9d6aa5a2f9b00dbdeb2c91508
joelshaw <jshaw@rvadesignsolutions.com>
2018-07-29 16:29:48 -0400

    Add conditional to render Content component on click

--- src/components/drawer.js
+++ src/components/drawer.js
@@ -5,36 +5,28 @@ import Content from './content';
 class Drawer extends Component {
    constructor(props){
        super(props);
-       this.state = {
-           active: false,
-       };
+       this.state = { show: true };
        this.state.value = "+";
 
        this.handleClick = this.handleClick.bind(this);
    }
 
     handleClick(event){
-        this.setState (drawerFunction => ({
-            isToggleOn: !drawerFunction.isToggleOn
-        }));
+        this.setState ({
+            isClosed: !this.state.isClosed
+        });
     }
-    drawer(props){
-        const isToggleOn = props.isToggleOn;
-        if(isToggleOn){
-            console.log('yes');
-        }
-        console.log('no');
-    }
-    render(){   
+
+    render(){
+
         return(
             <div className="drawerContainer">
                 <div className="drawerHeader">
                     <h1>{this.props.name}</h1>
-                    {this.state.isToggleOn ? 'NO' : 'YES'}
                     <button className="drawerState" value="+" onClick={this.handleClick}>{this.state.value}</button>
                 </div>
                 <div className="drawerContent">
-                    <Content />
+                    { this.state.isClosed && <Content /> }
                 </div>
             </div>
         );
